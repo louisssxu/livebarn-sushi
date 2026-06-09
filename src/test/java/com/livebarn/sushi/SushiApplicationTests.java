6496b76e1f12ecd35cde6ba4475015af752160f8
Louis Xu <lxu@livebarn.com>
2026-06-09 14:59:28 -0400

    init

---
+++ src/test/java/com/livebarn/sushi/SushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livebarn.sushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
